5ba043f8bfead46cd6d9caf86bc3fb63a8cc99bd
JoraBoraTatarstan <105365520+JoraBoraTatarstan@users.noreply.github.com>
2025-02-19 23:22:05 +0300

    Create graphicsview.h

---
+++ graphicsview.h
@@ -0,0 +1,28 @@
+#ifndef GRAPHICSVIEW_H
+#define GRAPHICSVIEW_H
+
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QMouseEvent>
+#include "lineitem.h"
+
+class GraphicsView : public QGraphicsView {
+    Q_OBJECT
+
+public:
+    GraphicsView(QWidget *parent = nullptr);
+    ~GraphicsView();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+private:
+    QGraphicsScene *scene;
+    LineItem *currentLine;
+    QPointF startPoint;
+    bool isDrawing;
+};
+
+#endif // GRAPHICSVIEW_H
