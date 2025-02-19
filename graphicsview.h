4a96cdfe7c47eb8d560d4aaed33a0a2c4bf18a49
JoraBoraTatarstan <105365520+JoraBoraTatarstan@users.noreply.github.com>
2025-02-19 23:50:19 +0300

    Update graphicsview.h

--- graphicsview.h
+++ graphicsview.h
@@ -17,12 +17,15 @@ protected:
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
 
 private:
     QGraphicsScene *scene;
     LineItem *currentLine;
     QPointF startPoint;
     bool isDrawing;
+    bool isPanning; // Режим перемещения сцены
+    QPoint panStartPos; // Начальная позиция курсора при перемещении
 };
 
 #endif // GRAPHICSVIEW_H
