ff981981fbafdef8f2fdb86f58846f7e7f232e36
JoraBoraTatarstan <105365520+JoraBoraTatarstan@users.noreply.github.com>
2025-02-19 23:22:53 +0300

    Create lineitem.h

---
+++ lineitem.h
@@ -0,0 +1,16 @@
+#ifndef LINEITEM_H
+#define LINEITEM_H
+
+#include <QGraphicsLineItem>
+#include <QPointF>
+
+class LineItem : public QGraphicsLineItem {
+public:
+    LineItem(const QPointF &start, const QPointF &end, QGraphicsItem *parent = nullptr);
+    void setEndPoint(const QPointF &end);
+
+private:
+    QPointF startPoint;
+};
+
+#endif // LINEITEM_H
